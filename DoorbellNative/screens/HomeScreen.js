b0fd0e6bd5e5a0f1a5eeca7d0aca7b67889952cc
Ophiun <damphousse.e@gmail.com>
2020-03-14 18:15:07 -0400

    For some reason the git overwrote and duplicated parts of file. Fixed and cleaned code

--- DoorbellNative/screens/HomeScreen.js
+++ DoorbellNative/screens/HomeScreen.js
@@ -1,47 +1,4 @@
-import React from 'react'; 
-import {View, Text, StyleSheet, Button} from 'react-native'; 
 
-import ImageButton from '../components/ImageButton';
-
-const HomeScreen = props => {
-
-    return(
-        <View style={styles.screen}>
-            
-            <View style={styles.buttonContainer}>
-                <ImageButton
-                    source={require('../assets/cam.png')}
-                    onPress={() => {props.navigation.navigate({routeName: 'Stream'})}}
-                />
-                <ImageButton
-                    source={require('../assets/lib.png')}
-                    onPress={() => {props.navigation.navigate({routeName: 'Video'})}}
-                />
-                <ImageButton
-                    source={require('../assets/cog.png')}
-                    onPress={() => {props.navigation.navigate({routeName: 'Settings'})}}
-                />
-            </View>
-        </View>
-    );
-}
-
-const styles = StyleSheet.create({
-    screen: {
-        flex: 1, 
-        padding: 10, 
-        alignItems: 'center' 
-    }, 
-    buttonContainer: {
-        flexDirection: 'column', 
-        justifyContent: 'space-around', 
-        marginTop: 20, 
-        width: 100, 
-        maxWidth: '80%'
-    },
-}); 
-
-export default HomeScreen; 
 import React from 'react'; 
 import {View, Text, StyleSheet, Button} from 'react-native'; 
 import io from 'socket.io-client'; 
@@ -89,7 +46,7 @@ export default class HomeScreen extends React.Component {
             <View style={styles.screen}>
                 <View style={styles.buttonContainer}>
                     <Text> {this.state.status}</Text>
-                    <Button title='CONNECT' onPress={() => this.dispatch('button_press', 0)}/>
+                    <Button title='CONNECT' onPress={() => this._dispatch('button_press', 0)}/>
                     <ImageButton
                         source={require('../assets/cam.png')}
                         onPress={() => {
